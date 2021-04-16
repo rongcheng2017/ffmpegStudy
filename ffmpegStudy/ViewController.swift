d6e8dd15f9729a68b084991417d7b526f12e8a32
windy <frcwindy@gmail.com>
2021-04-16 15:53:13 +0800

    first commit

--- ffmpegStudy/ViewController.swift
+++ ffmpegStudy/ViewController.swift
@@ -11,8 +11,17 @@ class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let btn = NSButton.init(title: "test", target: self, action: #selector(testBtnClick))
+        btn.frame = NSRect(x: 180, y: 120, width: 80, height: 40)
+        btn.bezelStyle = .rounded
+        btn.setButtonType(.pushOnPushOff)
+        self.view.addSubview(btn)
 
         // Do any additional setup after loading the view.
+    }
+    @objc
+    func testBtnClick(){
+        print("test btn click")
     }
 
     override var representedObject: Any? {
