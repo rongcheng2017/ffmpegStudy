af80e811506c3c9f5d00537b9a706b5044cc7916
windy <frcwindy@gmail.com>
2021-04-16 15:45:06 +0800

    Initial Commit

---
+++ ffmpegStudy/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ffmpegStudy
+//
+//  Created by 丰荣成 on 2021/4/16.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
